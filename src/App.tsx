2a9a133bfd9138409b98be1f6ce036c00dae7d3f
Brian <briannicolasdc@gmail.com>
2023-04-20 14:41:22 -0300

    add todo functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<string[]>([]);
 
   return (
     <div className="App">
       <div>
-        
+        <form onSubmit={(event) => {event.preventDefault(); setTodos([...todos, value]);}}>
+          <input
+            type="text"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button type="submit">Adicionar</button>
+          
+        </form>
+        <ul>
+          {
+            todos.map((todos, i) => (
+             <li key={i}>{todos}</li>
+            ))
+          }
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
