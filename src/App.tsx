479624fc9d0430f82f0cfb671c8a1dfcb5d3e4f8
Brian <briannicolasdc@gmail.com>
2023-05-20 17:47:52 -0300

    whenever a todo is checked, it is rendered with a strikethrough them

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { useState } from "react";
 import "./App.css";
 
+interface TodoItem {
+  id: string;
+  name: string;
+  isChecked: boolean;
+}
+
 const App: React.FC = () => {
-  const [value, setValue] = useState<string>("");
-  const [todos, setTodos] = useState<string[]>([]);
-  const deleteTodo = (index : Number) => {
-    setTodos(todos => {
-      return todos.filter((_, i) => i !== index);
-    })
+  const [newTodoDescription, setNewTodoDescription] = useState<string>("");
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+
+  const todoCompleted = (item: TodoItem) => {
+    todos.push(todos.splice(todos.indexOf(item), 1)[0]);
+  };
+
+  function onDeleteItem(item: TodoItem): void {
+    setTodos((prev) => prev.filter((listItem) => listItem.id !== item.id));
+  }
+
+  function onChangeItemName(item: TodoItem): void {
+    throw new Error("Function not implemented.");
+  }
+
+  function onCheckItem(item: TodoItem, isChecked: false): void {
+    throw new Error("Function not implemented.");
   }
 
   return (
@@ -17,58 +34,35 @@ const App: React.FC = () => {
           <form
             onSubmit={(event) => {
               event.preventDefault();
-              setTodos([...todos, value]);
+              setTodos([
+                ...todos,
+                {
+                  id: Math.random().toString(),
+                  name: newTodoDescription,
+                  isChecked: false,
+                },
+              ]);
             }}
           >
             <input
               type="text"
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
+              value={newTodoDescription}
+              onChange={(e) => setNewTodoDescription(e.target.value)}
             />
             <button type="submit">Adicionar</button>
           </form>
         </div>
         <div>
-        <ul>
-          {todos.map((todos, i) => (
-            <div>
-              <li key={i}>
-                {todos}
-                <button
-                  onClick={() => {
-                    return (
-                      <div>
-                        <form
-                          onSubmit={(event) => {
-                            event.preventDefault();
-                            setTodos([...todos, value]);
-                          }}
-                        >
-                          <input
-                            type="text"
-                            value={value}
-                            onChange={(e) => setValue(e.target.value)}
-                          />
-                          <button type="submit">Salvar</button>
-                        </form>
-                      </div>
-                    );
-                  }}
-                >
-                  edit
-                </button>
-                <button
-                  onClick={() => {
-                    return deleteTodo(i);
-                  }}
-                >
-                  del
-                </button>
-                <input type="checkbox" />
-              </li>
-            </div>
-          ))}
-        </ul>
+          <ul>
+            {todos.map((todo, i) => (
+              <TodoItem
+                item={todo}
+                onChangeName={onChangeItemName}
+                onDelete={onDeleteItem}
+                onCheck={onCheckItem}
+              />
+            ))}
+          </ul>
         </div>
       </div>
     </div>
@@ -76,3 +70,53 @@ const App: React.FC = () => {
 };
 
 export default App;
+
+interface TodoItemProps {
+  item: TodoItem;
+  onDelete: (item: TodoItem) => void;
+  onChangeName: (item: TodoItem) => void;
+  onCheck: (item: TodoItem, isChecked: false) => void;
+}
+
+function TodoItem({
+  item,
+  onDelete,
+  onChangeName,
+  onCheck,
+}: TodoItemProps): JSX.Element {
+  const [isInEditMode, setIsInEditMode] = useState(false);
+  const [isCheck, setIsCheck] = useState(false);
+
+  const editing = (item: TodoItem) => {
+    return <div></div>;
+  };
+
+  return (
+    <div>
+      <li>
+        {isInEditMode ? (
+          <div>
+            <form
+              action="submit"
+              onSubmit={(event) => {event.preventDefault; }}
+            ></form>
+            <input
+              type="text"
+              value={item.name}
+              onChange={
+                () => {}
+            }
+            />
+            <button type="submit">save</button>
+            <button onClick={() => setIsInEditMode(false)}>cancel</button>
+          </div>
+        ) : (
+          <>{isCheck ? <s>{item.name}</s> : <>{item.name}</>}</>
+        )}
+        <button onClick={() => setIsInEditMode(true)}>edit</button>
+        <button onClick={() => onDelete(item)}>del</button>
+        <input type="checkbox" onChange={() => setIsCheck(!isCheck)} />
+      </li>
+    </div>
+  );
+}
